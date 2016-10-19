7cf1f46443f062a6c2f929421d13331c9a5e37e0
KatrukOV <KatrukOV@gmail.com>
2016-10-19 19:50:49 +0300

    add google style

--- src/com/katruk/Main.java
+++ src/com/katruk/Main.java
@@ -1,50 +1,51 @@
 package com.katruk;
 
-import com.katruk.controller.EncodingText;
 import com.katruk.controller.Controller;
+import com.katruk.controller.EncodingText;
 import com.katruk.model.entity.Data;
 import com.katruk.model.logic.Operation;
 import com.katruk.model.logic.OperationImp;
 import com.katruk.model.logic.Parser;
 import com.katruk.model.logic.ParserImp;
 import com.katruk.veiw.Renderer;
+
 import java.io.IOException;
 import java.util.List;
 
 public class Main {
-	public static final String PATH_INPUT = "Input.txt";
-	public static final String PATH_OUTPUT = "Output.txt";
 
-	public static void main(String[] args) throws IOException {
+  public static final String PATH_INPUT = "Input.txt";
+  public static final String PATH_OUTPUT = "Output.txt";
 
-		Controller controller = new Controller();
-		Renderer renderer = new Renderer();
-		Parser parser = new ParserImp();
-		Operation operation = new OperationImp();
+  public static void main(String[] args) throws IOException {
 
-		String string = controller.readFile(PATH_INPUT, EncodingText.CP1251);
+    Controller controller = new Controller();
+    Renderer renderer = new Renderer();
+    Parser parser = new ParserImp();
+    Operation operation = new OperationImp();
 
-		renderer.showLn(string);
+    String string = controller.readFile(PATH_INPUT, EncodingText.CP1251);
 
-		renderer.showLn("		1_________________");
+    renderer.showLn(string);
 
-		Data book = parser.parserAll(string);
-		renderer.showLn(book.toString());
+    renderer.showLn("		1_________________");
 
-		renderer.showLn("		2________________");
+    Data book = parser.parserAll(string);
+    renderer.showLn(book.toString());
 
-		List<String> words = operation.getAllWord(book);
-		for (String word : words) {
-			renderer.showLn(word);
-		}
+    renderer.showLn("		2________________");
 
-		renderer.showLn("		3_________________");
-		String sortWord = operation.sortWord(words);
+    List<String> words = operation.getAllWord(book);
+    for (String word : words) {
+      renderer.showLn(word);
+    }
 
-		renderer.showLn(sortWord);
-		string = words.toString();
+    renderer.showLn("		3_________________");
+    String sortWord = operation.sortWord(words);
 
-		controller.writeFile(PATH_OUTPUT, EncodingText.Utf8, string);
+    renderer.showLn(sortWord);
+    string = words.toString();
 
-	}
+    controller.writeFile(PATH_OUTPUT, EncodingText.Utf8, string);
+  }
 }
